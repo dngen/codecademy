9b2579ec2ca8f9bf3fb0b96186ba4728b23ef2a4
DeEn <35757294+dngen@users.noreply.github.com>
2018-04-22 02:50:50 +0700

    Update KelvinWeather.js

--- KelvinWeather.js
+++ KelvinWeather.js
@@ -1,9 +1,14 @@
-//put Kelvin eg. forecast today is 294 Kelvin.
-let kelvin = 0;
+//forecast today is 294 Kelvin.
+let kelvin = 294;
 //convert Kelvin to Celsius by subtracting 273 from the kelvin
 let celsius = kelvin-273;
+//Newton
+let newton = celsius * (33/100)
+//round down Newton
+newton = Math.floor(newton);
 //calculate Fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
 //round down the Fahrenheit temperature.
 fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+//result
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit., is ${newton} Newton`);
